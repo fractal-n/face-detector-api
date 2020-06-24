50c9b56114aebfa4a18c2d1bb98f9a775995628e
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-24 13:04:51 -0600

    add port env

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get("/profile/:id", (req, res) => handleProfileGet(req, res, db));
 app.patch("/image", (req, res) => handleImage(req, res, db));
 app.post("/image", (req, res) => handleClarifaiApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
