0798b2840797f534b535e6b19eecb908839649e9
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-24 20:47:27 -0600

    add database config for Heroku

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ import { handleImage, handleClarifaiApiCall } from "./controllers/image.js";
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "UXfqA7&!5$c&",
-    database: "face-detector",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
