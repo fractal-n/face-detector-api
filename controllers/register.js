3de9dcaaf9504c65a64bf7f37086389b6e330fb0
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-24 22:54:22 -0600

    remove debug lines

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,5 @@
 export function handleRegister(req, res, db, bcrypt, saltRounds) {
   const { email, name, password } = req.body;
-  console.log(req.body);
 
   if (!email || !name || !password) {
     return res.status(400).json("incorrect form submission");
